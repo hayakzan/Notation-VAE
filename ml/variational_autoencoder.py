73fe344f23cd1f06b41e16d11403cf344b7e1374
hayakzan <61164329+hayakzan@users.noreply.github.com>
2021-10-16 16:50:53 -0700

    Update variational_autoencoder.py

--- ml/variational_autoencoder.py
+++ ml/variational_autoencoder.py
@@ -1,139 +1,131 @@
 import os
 import pickle
 
+import matplotlib.pyplot as plt
+import moviepy.editor as mpyi
 import numpy as np
-from keras import backend as K
-from keras.callbacks import ModelCheckpoint, LearningRateScheduler
-from keras.layers import (
-    Activation,
-    BatchNormalization,
-    Conv2D,
-    Conv2DTranspose,
-    Dense,
-    Flatten,
-    Input,
-    Lambda,
-    LeakyReLU,
-    Reshape,
-)
-from keras.models import Model
-from tensorflow.keras.optimizers import Adam
+from keras.datasets import mnist
+from PIL import Image
 
 from ml.constants import DEFAULT_MODEL_PATH
+from ml.variational_autoencoder import VariationalAutoencoder
 
 
-class VariationalAutoencoder:
-    def __init__(
-        self, input_dim, latent_dim, encoder_params=[], decoder_params=[],
-    ):
-        self.input_dim = input_dim
-        self.latent_dim = latent_dim
-        self.encoder_params = encoder_params
-        self.decoder_params = decoder_params
+def load_mnist():
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-        self._build()
+    x_train = x_train.astype("float32") / 255.0
+    x_train = x_train.reshape(x_train.shape + (1,))
+    x_test = x_test.astype("float32") / 255.0
+    x_test = x_test.reshape(x_test.shape + (1,))
 
-    def _build(self):
-        self._build_encoder()
-        self._build_decoder()
-        self.model = Model(self.encoder_input, self.decoder_model(self.encoder_output))
+    return (x_train, y_train), (x_test, y_test)
 
-    def save(self, output_path=DEFAULT_MODEL_PATH):
-        params = [
-            self.input_dim,
-            self.latent_dim,
-            self.encoder_params,
-            self.decoder_params,
-        ]
 
-        if not os.path.exists(output_path):
-            os.makedirs(output_path)
+def load_gestures(path="dataset", sizeX=64, sizeY=64):
+    gestures = {}
+    for fname in os.listdir(path):
+        if not fname.startswith('.'): ##against DS_Store shit
+            with Image.open(os.path.join(path, fname)) as raw:
+                img = raw
+                img = raw.resize((sizeX, sizeY), Image.ANTIALIAS)
+                img = np.divide(np.array(img), 255)
+                gestures[fname] = img
+    return gestures
 
-        with open(os.path.join(output_path, "params.pkl"), "wb") as f:
-            pickle.dump(params, f)
 
-    def _build_encoder(self):
-        self.encoder_input = Input(shape=self.input_dim, name="encoder_input")
+def plot_random_gestures(decoder, n_to_show=10):
+    size = int(decoder.inputs[0].shape[1])
+    random_vectors = np.random.normal(loc=0, scale=1, size=(n_to_show, size))
 
-        x = self.encoder_input
+    images = decoder.predict(random_vectors)
 
-        for i, params in enumerate(self.encoder_params):
-            x = Conv2D(**params, padding="same", name=f"encoder_conv_{i}")(x)
-            x = BatchNormalization()(x)
-            x = LeakyReLU()(x)
+    fig = plt.figure(figsize=(15, 3))
+    fig.subplots_adjust(hspace=0.4, wspace=0.4)
 
-        self.shape_before_flattening = K.int_shape(x)[1:]
+    for i in range(n_to_show):
+        img = images[i].squeeze()
+        sub = fig.add_subplot(1, n_to_show, i + 1)
+        sub.axis("off")
+        sub.imshow(img)
 
-        x = Flatten()(x)
 
-        # handle the variational bit
-        self.mu = Dense(self.latent_dim, name="mu")(x)
-        self.log_var = Dense(self.latent_dim, name="log_var")(x)
+def plot_reconstructed_images(data, encoder, decoder, n_to_show=10):
+    example_idx = np.random.choice(range(len(data)), n_to_show)
+    example_images = data[example_idx]
 
-        self.encoder_mu_log_var = Model(self.encoder_input, (self.mu, self.log_var))
+    latent_coords = encoder.predict(example_images)
+    reconst_images = decoder.predict(latent_coords)
 
-        def sampling(args):
-            mu, log_var = args
-            epsilon = K.random_normal(shape=K.shape(mu), mean=0.0, stddev=1.0)
-            return mu + K.exp(log_var / 2) * epsilon
+    fig = plt.figure(figsize=(15, 3))
+    fig.subplots_adjust(hspace=0.4, wspace=0.4)
 
-        self.encoder_output = Lambda(sampling, name="encoder_output")(
-            [self.mu, self.log_var]
+    for i in range(n_to_show):
+        img = example_images[i].squeeze()
+        sub = fig.add_subplot(2, n_to_show, i + 1)
+        sub.axis("off")
+        sub.imshow(img)
+
+    for i in range(n_to_show):
+        img = reconst_images[i].squeeze()
+        sub = fig.add_subplot(2, n_to_show, i + n_to_show + 1)
+        sub.axis("off")
+        sub.imshow(img)
+
+
+def generate_morph_images(image_1, image_2, encoder, decoder, n_steps):
+    vec1 = encoder.predict(np.array([image_1]))
+    vec2 = encoder.predict(np.array([image_2]))
+
+    morph_vecs = [
+        np.add(
+            np.multiply(vec1, (n_steps - 1 - i) / (n_steps - 1)),
+            np.multiply(vec2, i / (n_steps - 1)),
         )
-        self.encoder_model = Model(self.encoder_input, self.encoder_output)
+        for i in range(n_steps)
+    ]
 
-    def _build_decoder(self):
-        decoder_input = Input(shape=(self.latent_dim,), name="decoder_input")
+    return [decoder.predict(vec) for vec in morph_vecs]
 
-        x = Dense(np.prod(self.shape_before_flattening))(decoder_input)
-        x = Reshape(self.shape_before_flattening)(x)
 
-        for i, params in enumerate(self.decoder_params):
-            x = Conv2DTranspose(**params, padding="same", name=f"decoder_conv_t_{i}")(x)
-            if i < len(self.decoder_params) - 1:
-                x = BatchNormalization()(x)
-                x = LeakyReLU()(x)
+def plot_morph_images(image_1, image_2, encoder, decoder, n_steps=10):
+    morph_images = generate_morph_images(image_1, image_2, encoder, decoder, n_steps)
 
-        decoder_output = Activation("sigmoid")(x)
+    #fig = plt.figure(figsize=(15, 3))
+    fig = plt.figure()
+    fig.subplots_adjust(hspace=0.4, wspace=0.4)
 
-        self.decoder_model = Model(decoder_input, decoder_output)
+    for i, morph_img in enumerate(morph_images):
+        img = morph_img.squeeze()
+        sub = fig.add_subplot(1, n_steps, i + 1)
+        sub.axis("off")
+        plt.imsave('foo'+str(i)+'.png', img)
+        plt.close()
+        sub.imshow(img)
 
-    def compile_model(self, lr, r_loss_factor):
-        def vae_r_loss(y_true, y_pred):
-            r_loss = K.mean(K.square(y_true - y_pred), axis=[1, 2, 3])
-            return r_loss_factor * r_loss
 
-        def vae_kl_loss(y_true, y_pred):
-            kl_loss = -0.5 * K.sum(
-                1 + self.log_var - K.square(self.mu) - K.exp(self.log_var), axis=1
-            )
-            return kl_loss
+def animate_morph_images(image_1, image_2, encoder, decoder, n_steps=60, loop=True):
+    morph_images = [
+        np.multiply(f[0], 255)
+        for f in generate_morph_images(image_1, image_2, encoder, decoder, n_steps)
+    ]
 
-        def vae_loss(y_true, y_pred):
-            r_loss = vae_r_loss(y_true, y_pred)
-            kl_loss = vae_kl_loss(y_true, y_pred)
-            return r_loss + kl_loss
-
-        self.model.compile(
-            optimizer='adam', loss=vae_loss, metrics=[vae_r_loss, vae_kl_loss]
+    if loop:
+        morph_images = (
+            int(n_steps / 5) * [morph_images[0]]
+            + morph_images
+            + int(n_steps / 5) * [morph_images[-1]]
+            + morph_images[::-1]
         )
 
-    def train(self, train_data, batch_size, epochs, path=DEFAULT_MODEL_PATH):
-        if not os.path.exists(path):
-            os.makedirs(path)
+    return mpy.ImageSequenceClip(morph_images, fps=30)
 
-        checkpoint = ModelCheckpoint(
-            ##os.path.join(path, "weights.h5"),
-            filepath="/content/gdrive/My Drive/weights.h5",
-            save_weights_only=True,
-            verbose=1,
-        )
 
-        self.model.fit(
-            train_data,
-            train_data,
-            batch_size=batch_size,
-            shuffle=True,
-            epochs=epochs,
-            callbacks=[checkpoint],
-        )
+def load_model(path=DEFAULT_MODEL_PATH):
+    with open(os.path.join(path, "params.pkl"), "rb") as f:
+        params = pickle.load(f)
+
+    vae = VariationalAutoencoder(*params)
+    vae.model.load_weights(os.path.join(path, "weights.h5"))
+
+    return vae
